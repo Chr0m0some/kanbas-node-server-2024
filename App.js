1d2954d3df8791390e5aca01376cf9aead7e9ac0
--replace-all <leework054@gmail.com>
2024-07-15 18:44:07 -0500

    made modules async

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import express from "express";
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import cors from "cors";
 
 const app = express();
@@ -14,5 +15,6 @@ app.get("/lab5/welcome", (req, res) => res.send("Welcome to the Server"));
 Hello(app);
 Lab5(app); 
 CourseRoutes(app);
+ModuleRoutes(app);
 
 app.listen(process.env.PORT || 4000);
