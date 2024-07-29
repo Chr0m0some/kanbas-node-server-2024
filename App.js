e9602c503e7d9c773fbff5d6554507d8ac8d8527
--replace-all <leework054@gmail.com>
2024-07-29 14:53:20 -0500

    finished last wks lecture

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import UserRoutes from "./User/routes.js";
 import cors from "cors";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-suf";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+UserRoutes(app);
 app.get("/lab5/welcome", (req, res) => res.send("Welcome to the Server"));
 
 Hello(app);
