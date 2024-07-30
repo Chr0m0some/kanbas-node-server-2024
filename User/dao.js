058a5f6613d286cb932bd859a929118f7c4f34a4
--replace-all <leework054@gmail.com>
2024-07-30 11:15:33 -0500

    adding cookies and account routes

--- User/dao.js
+++ User/dao.js
@@ -2,7 +2,7 @@ import model from "./model.js";
 export const createUser = (user) => {
   delete user._id;
   return model.create(user);
-}; 
+};
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>
@@ -19,4 +19,5 @@ export const findUsersByPartialName = (partialName) => {
     $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
   });
 };
-// Figure out a way to make the logic work for getting users by role and partial name
+
+
