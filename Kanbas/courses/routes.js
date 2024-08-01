54d8e48e7a33e3e32aee426311d439e7baa339c3
--replace-all <leework054@gmail.com>
2024-08-01 10:40:36 -0500

    got rid of comments

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,42 +1,5 @@
-import Database from "../Database/index.js";
 import * as dao from "./dao.js";
 export default function CourseRoutes(app) {
-  // app.get("/api/courses", (req, res) => {
-  //   const courses = Database.courses;
-  //   res.send(courses);
-  // });
-  // app.post("/api/courses", (req, res) => {
-  //   const course = { ...req.body, _id: new Date().getTime().toString() };
-  //   Database.courses.push(course);
-  //   res.send(course);
-  // });
-  // app.delete("/api/courses/:id", (req, res) => {
-  //   const { id } = req.params;
-  //   const index = Database.courses.findIndex((c) => c._id === id);
-  //   if (index === -1) {
-  //     res
-  //       .status(404)
-  //       .json({ message: `Unable to delete course with ID ${id}` });
-  //     return;
-  //   }
-  //   Database.courses.splice(index, 1);
-  //   res.sendStatus(204);
-  // });
-  // app.put("/api/courses/:id", (req, res) => {
-  //   const { id } = req.params;
-  //   const index = Database.courses.findIndex((c) => c._id === id);
-  //   if (index === -1) {
-  //     res
-  //       .status(404)
-  //       .json({ message: `Unable to delete course with ID ${id}` });
-  //     return;
-  //   }
-  //   const course = req.body;
-  //   Database.courses = Database.courses.map((c) =>
-  //     c._id === id ? { ...c, ...course } : c
-  //   );
-  //   res.sendStatus(204);
-  // });
   const findAllCourses = async (req, res) => {
     const courses = await dao.findAllCourses();
     res.json(courses);
@@ -53,9 +16,9 @@ export default function CourseRoutes(app) {
   const deleteCourse = async (req, res) => {
     const status = await dao.deleteCourse(req.params.cid);
     res.json(status);
-  }
+  };
   app.get("/api/courses", findAllCourses);
   app.post("/api/courses", createCourse);
   app.put("/api/courses/:cid", updateCourse);
-  app.delete("/api/courses/:cid", deleteCourse)
+  app.delete("/api/courses/:cid", deleteCourse);
 }
